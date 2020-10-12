9c112e383bfad6d8b4bb4ac26fe1b42b34b4e989
Sarthak Arora <sarthakarora1503@gmail.com>
2020-10-13 04:42:38 +0530

    home.js renamed to app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+let express = require("express"),
+    mongoose = require("mongoose"),
+    bodyParser = require("body-parser"),
+    app = express();
+
+app.set("view engine","ejs");
+bodyParser.urlencoded({extended: true});
+app.connect("mongodb://localhost/obsdb");
+
+let Accounts = new mongoose.Schema({
+    name: String,
+    address: String,
+    contact: Number,
+    email: String,
+    account_no: Number,
+    current_balance: Number,
+    transactions: {
+        amount: Number,
+        typeOftransac: String,
+        date: Date
+    }
+});
+
+app.listen(80,"127.0.0.1",(req,res)=> {
+    console.log("THE SERVER IS LISTENING!!");
+});
