595198e88eb74fa66d6d2a6f1409ffb7317e6035
Sarthak Arora <sarthakarora1503@gmail.com>
2020-10-25 00:37:54 +0530

    Japnit's hardwork restored

--- app.js
+++ app.js
@@ -2,22 +2,17 @@ let express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser"),
     // nodemailer = require("nodemailer"),
-    bcrypt = require("bcrypt"),
     app = express();
-
 const port = 80;
-
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
-
 mongoose.connect("mongodb://localhost/obsdb",{useNewUrlParser: true,useUnifiedTopology:true});
-
+//console.log(mongoose.connect.readyState);
 let transactionSchema = new mongoose.Schema({
     amount: Number,
     typeOftransac: String,
     dateANDtime : Date
 })
-
 let AccountsSchema = new mongoose.Schema({
     name: String,
     address: String,
@@ -32,122 +27,65 @@ let AccountsSchema = new mongoose.Schema({
     transactions:[transactionSchema]
 });
 let Accounts = mongoose.model("Accounts",AccountsSchema);
-
-
+//  Accounts.create(
+//      { name: "japnit", 
+//        transactions:[ 
+//        {
+//          amount: 15000,
+//          typeOftransac: "deposit",
+//          dateANDtime: "2020-10-17"
+//        },
+//        {
+//          amount:23000,
+//          typeOftransac: "withdrawl",
+//          dateANDtime: "2020-10-19"
+//        }
+//      ]},
+//      function(err,account){
+//          if(err){
+//              console.log(err);
+//          }
+//          else{
+//              console.log("Account Created");
+//              console.log(account);
+//          }
+//      });
 app.get("/",(req,res)=> {
      res.render('home');
 });
-
-let signal = 0,
-    details,
-    logindetails,
-    saltRounds = 10;
-
+let signal=0;
 app.get("/login",(req,res)=> {
-    res.render("login",{signal: signal});
+    signal = 1;
+    res.render("login");
 });
-
-app.get("/signup",(req,res)=> {
-    res.render("signup",{signal: signal});
-});
-
-app.post("/signup",(req,res)=> {
-    let email = req.body.email,
-        pass = req.body.password;
-    
-    bcrypt.genSalt(saltRounds, function(err,salt) {
-        bcrypt.hash(pass, salt, function(err,hash) {
-            if(err)
-                console.log(err);
-            else
-                pass = hash;
-        });
-    });
-    
-    logindetails = {
-        email: email, 
-        password: pass
-    };
-
-    Accounts.create(logindetails, (err,logindetails)=> {
-        if(err)
-            console.log(err);
-        else
-            console.log(logindetails);
-    });
-
-    console.log("Password created successfully.");
-    res.redirect("/login");
-});
-
-app.get("/accountactivity",(req,res)=> {
+app.get("/transaction",(req,res)=> {
     if(signal == 0)
-    {
-        alert("You need to be logged in first!!");
         res.render("login");
-    }
     else
-    {
-        res.render("accountactivity",{Accounts:Accounts, signal:signal});   
-    }
+        res.render("transaction",{Accounts:Accounts, signal:signal});   
 });
-
-app.post("/accountactivity",(req,res)=> {
-
+let details1, details2;
+app.post("/transaction",(req,res)=> {
     let email = req.body.email,
-        pass = req.body.password;
+        mobile = req.body.Mobileno1;
     
-    logindetails = {
+    details1 = {
         email: email,
-        password: pass
-    };
-
-    Accounts.find({email: email},(err,record)=> {
-        if(err)
-            console.log(err);
-        else   
-        {
-            console.log(record);
-
-            if(record.estimatedDocumentCount() == 0)
-            {
-                console.log("LOGIN AGAIN!!");
-                res.render("login");
-            }
-            else
-            {
-                bcrypt.compare(pass, record.password, (err,result)=> {
-                    if(result)
-                    {
-                        console.log("LOGIN DONE!!");
-                        res.render("transaction",{signal: signal, check: record});
-                        signal = 1;
-                    }
-                    else
-                    {
-                        console.log("LOGIN PENDING!!");
-                        res.render("login");
-                    }
-                });
-            }
-        }
-    });
+        Mobileno1: mobile
+    }
+    details2 = {
+        email: email,
+        Mobileno2: mobile
+    }
+    res.render("transaction");
 });
-
+app.get("/personaldetails",(req,res)=> {
+    res.render("personaldetails",{Accounts:Accounts, signal:signal, details1:details1, details2:details2});
+});
 app.get("/create_acc",(req,res)=> {
-    if(signal == 0)
-    {
-        alert("You need to be logged in first!!");
-        res.redirect("/login");
-    }
-    else
-    {
-        res.render("openaccount");
-    }
+    res.render('openaccount');
 });
-
 app.post("/create_acc",(req,res)=> {
-
      var name =  req.body.name;
      var address =  req.body.address;
      var email = req.body.email;
@@ -155,56 +93,60 @@ app.post("/create_acc",(req,res)=> {
      var Mobileno1 = req.body.Mobileno1;
      var Mobileno2 = req.body.Mobileno2;
      var Phoneno = req.body.Phoneno;
-     
-     var details = {
-        name:name,
-        address:address,
-        email: email,
-        gender:gender,
-        Mobileno1:Mobileno1,
-        Mobileno2:Mobileno2,
-        Phoneno:Phoneno
-    };
-
-    Accounts.update(logindetails,{$set: details},(err,account_created)=> {
+     var details = {name:name,address:address,
+                    email:email,gender:gender,
+                    Mobileno1:Mobileno1,Mobileno2:Mobileno2,
+                    Phoneno:Phoneno};
+    Accounts.create(details,(err,account_created)=> {
         if(err)
         {
             console.log(err);
         }
-        else
-        {
+        else{
             console.log('Account Created');
+            res.render("newpass");
         }
     });
-    res.redirect("/accountactivity");
+    // var transporter = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     secure: false,
+    //     auth: {
+    //         user: email
+    //     }
+    // });
+    // var mailOptions = {
+    //     from: 'sarthakarora1503@gmail.com',
+    //     to: email,
+    //     subject: 'Set your password',
+    //     html: "<a href='/newpass'>Click here to set your online account password</a>"
+    // };
+    // transporter.sendMail(mailOptions, function(error, info){
+    //     if (error) {
+    //         console.log(error);
+    //     } else {
+    //         console.log('Email sent: ' + info.response);
+    //     }
+    // });
 });
-
-app.get("/transaction",(req,res)=> {
-    if(signal == 0)
-    {
-        res.render("login");
-    } 
-    else    
-    {
-        res.render("transaction",{Accounts:Accounts, signal:signal});  
-    }
+app.get("/newpass",(req,res)=> {
+    res.render("newpass");
 });
-
-app.get("/personaldetails",(req,res)=> {
-    Accounts.findOne(details, (err,record) => { 
-        
-        if(err)
-            console.log(err);
-        else 
-            console.log(record);
-        
-        res.render("personaldetails",{Accounts:Accounts, signal:signal, record:record});
-    });
+app.post("/newpass",(req,res)=> {
     
+    let temp = Accounts;
+    temp.find({}).sort();
+    let pass = req.body.password,
+        t = temp.findOne();
+    let name = t["name"],
+        email = t["email"];
+    Accounts.update({name: name, email: email},{$set: {password: pass}});
+    console.log("Password created successfully.");
+    res.redirect("/");
 });
 
 app.get("/view",(req,res)=> {
     Accounts.find({},function(err,alltransaction){
+    Accounts.findOne({name:"japnit"},function(err,alltransaction){
         if(err)
         {
             console.log(err);
@@ -213,8 +155,57 @@ app.get("/view",(req,res)=> {
         {
             res.render("view",{Account:alltransaction});
         }    
-    })
+    });
 });
+});
+
+app.post("/view",(req,res)=>{
+    var total;
+    var amt = req.body.trans_amount;
+    amt = parseInt(amt); 
+    var type = req.body.trans_type;
+    var date = new Date()
+   Accounts.findOne({name:"japnit"},function(err,alltransaction){
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+          total = alltransaction.total_amount;
+          total = parseInt(total);
+         if(type=="withdrawl") 
+         {
+            if(amt<total-1000)
+            {
+               Accounts.update({name:"japnit"},{$set:{total_amount:total-amt},$push:{transactions:[{amount:amt,typeOftransac:type,dateANDtime:date}]}},function(err,transaction){
+              if(err)
+               { 
+                  console.log(err);
+               }
+                });
+            res.redirect("view");
+            }
+            else
+            {
+              res.render("alert")
+            }
+         }
+         else if(type=="deposit")
+         {
+            Accounts.update({name:"japnit"},{$set:{total_amount:(total+amt)},$push:{transactions:[{amount:amt,typeOftransac:type,dateANDtime:date}]}},function(err,transaction){
+                if(err)
+                 { 
+                    console.log(err);
+                 }
+                  });
+              res.redirect("view");
+         }  
+        }
+    });   
+
+
+}); 
 
 app.listen(port,()=> {
     console.log("THE SERVER IS LISTENING!!");
