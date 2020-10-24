08f25beede0091ede0bc77296923b42b4b5d1bad
Sarthak Arora <sarthakarora1503@gmail.com>
2020-10-24 17:52:09 +0530

    changes related all the files in views directory

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let express = require("express"),
     mongoose = require("mongoose"),
     bodyParser = require("body-parser"),
     // nodemailer = require("nodemailer"),
+    bcrypt = require("bcrypt"),
+    nodemon = require("nodemon"),
     app = express();
 
 const port = 80;
@@ -10,7 +12,6 @@ app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost/obsdb",{useNewUrlParser: true,useUnifiedTopology:true});
-//console.log(mongoose.connect.readyState);
 
 let transactionSchema = new mongoose.Schema({
     amount: Number,
@@ -33,137 +34,169 @@ let AccountsSchema = new mongoose.Schema({
 });
 let Accounts = mongoose.model("Accounts",AccountsSchema);
 
-//  Accounts.create(
-//      { name: "japnit", 
-//        transactions:[ 
-//        {
-//          amount: 15000,
-//          typeOftransac: "deposit",
-//          dateANDtime: "2020-10-17"
-//        },
-//        {
-//          amount:23000,
-//          typeOftransac: "withdrawl",
-//          dateANDtime: "2020-10-19"
-//        }
-//      ]},
-//      function(err,account){
-//          if(err){
-//              console.log(err);
-//          }
-//          else{
-//              console.log("Account Created");
-//              console.log(account);
-//          }
-//      });
 
 app.get("/",(req,res)=> {
      res.render('home');
 });
 
-let signal=0;
+let signal = 0,
+    details,
+    logindetails,
+    saltRounds = 10;
+
 app.get("/login",(req,res)=> {
-    signal = 1;
-    res.render("login");
+    res.render("login",{signal:signal});
+});
+
+app.get("/signup",(req,res)=> {
+    res.render("signup",{signal: signal});
+});
+
+app.post("/signup",(req,res)=> {
+    let email = req.body.email,
+        pass = req.body.password;
+    
+    bcrypt.genSalt(saltRounds, function(err,salt) {
+        bcrypt.hash(pass, salt, function(err,hash) {
+            if(err)
+                console.log(err);
+            else
+                pass = hash;
+        });
+    });
+    
+    logindetails = {
+        email: email, 
+        password: pass
+    };
+
+    Accounts.create(logindetails, (err,logindetails)=> {
+        if(err)
+            console.log(err);
+        else
+            console.log(logindetails);
+    });
+
+    console.log("Password created successfully.");
+    res.redirect("/login");
+});
+
+app.get("/accountactivity",(req,res)=> {
+    if(signal == 0)
+    {
+        res.render("login");
+    }
+    else
+    {
+        res.render("accountactivity",{Accounts:Accounts, signal:signal});   
+    }
+});
+
+app.post("/accountactivity",(req,res)=> {
+
+    let email = req.body.email,
+        pass = req.body.password;
+    
+    logindetails = {
+        email: email,
+        password: pass
+    };
+
+    Accounts.find({email: email},(err,record)=> {
+        if(err)
+            console.log(err);
+        else   
+        {
+            console.log(record);
+
+            if(record.estimatedDocumentCount() == 0)
+            {
+                console.log("LOGIN AGAIN!!");
+                res.render("login");
+            }
+            else
+            {
+                bcrypt.compare(pass, record.password, (err,result)=> {
+                    if(result)
+                    {
+                        console.log("LOGIN DONE!!");
+                        res.render("transaction",{signal: signal, check: record});
+                        signal = 1;
+                    }
+                    else
+                    {
+                        console.log("LOGIN PENDING!!");
+                        res.render("login");
+                    }
+                });
+            }
+        }
+    });
 });
 
 app.get("/transaction",(req,res)=> {
     if(signal == 0)
+    {
         res.render("login");
-    else
-        res.render("transaction",{Accounts:Accounts, signal:signal});   
-});
-
-let details1, details2;
-app.post("/transaction",(req,res)=> {
-    let email = req.body.email,
-        mobile = req.body.Mobileno1;
-    
-    details1 = {
-        email: email,
-        Mobileno1: mobile
+    } 
+    else    
+    {
+        res.render("transaction",{Accounts:Accounts, signal:signal});  
     }
-
-    details2 = {
-        email: email,
-        Mobileno2: mobile
-    }
-    res.render("transaction");
 });
 
 app.get("/personaldetails",(req,res)=> {
-    res.render("personaldetails",{Accounts:Accounts, signal:signal, details1:details1, details2:details2});
+    Accounts.findOne(details, (err,record) => { 
+        
+        if(err)
+            console.log(err);
+        else 
+            console.log(record);
+        
+        res.render("personaldetails",{Accounts:Accounts, signal:signal, record:record});
+    });
+    
 });
 
 app.get("/create_acc",(req,res)=> {
-    res.render('openaccount');
+    if(signal == 0)
+    {
+        res.render("login");
+    }
+    else
+    {
+        res.render("openaccount");
+    }
 });
 
 app.post("/create_acc",(req,res)=> {
 
      var name =  req.body.name;
      var address =  req.body.address;
-     var email = req.body.email;
      var gender = req.body.gender;
      var Mobileno1 = req.body.Mobileno1;
      var Mobileno2 = req.body.Mobileno2;
      var Phoneno = req.body.Phoneno;
-     var details = {name:name,address:address,
-                    email:email,gender:gender,
-                    Mobileno1:Mobileno1,Mobileno2:Mobileno2,
-                    Phoneno:Phoneno};
+     var details = {
+        name:name,
+        address:address,
+        gender:gender,
+        Mobileno1:Mobileno1,
+        Mobileno2:Mobileno2,
+        Phoneno:Phoneno
+    };
 
-    Accounts.create(details,(err,account_created)=> {
+    Accounts.update(logindetails,{$set: details},(err,account_created)=> {
         if(err)
         {
             console.log(err);
         }
-        else{
+        else
+        {
             console.log('Account Created');
-            res.render("newpass");
         }
     });
-
-    // var transporter = nodemailer.createTransport({
-
-    //     service: 'gmail',
-    //     secure: false,
-    //     auth: {
-    //         user: email
-    //     }
-    // });
-
-    // var mailOptions = {
-    //     from: 'sarthakarora1503@gmail.com',
-    //     to: email,
-    //     subject: 'Set your password',
-    //     html: "<a href='/newpass'>Click here to set your online account password</a>"
-    // };
-
-    // transporter.sendMail(mailOptions, function(error, info){
-    //     if (error) {
-    //         console.log(error);
-    //     } else {
-    //         console.log('Email sent: ' + info.response);
-    //     }
-    // });
-});
-
-app.get("/newpass",(req,res)=> {
-    res.render("newpass");
-});
-
-app.post("/newpass",(req,res)=> {
-    
-    let temp = Accounts;
-    temp.find({}).sort();
-    let pass = req.body.password,
-        t = temp.findOne();
-    let name = t["name"],
-        email = t["email"];
-    Accounts.update({name: name, email: email},{$set: {password: pass}});
-    console.log("Password created successfully.");
-    res.redirect("/");
+    res.redirect("/accountactivity");
 });
 
 app.get("/view",(req,res)=> {
