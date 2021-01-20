d14fb7e38b3cd35961dcd43e2b40cf241efef053
Sarthak Arora <sarthakarora1503@gmail.com>
2021-01-21 03:03:20 +0530

    removed unwanted files

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import registerServiceWorker from "./registerServiceWorker";
-import app from "./app";
-import Hello from "./Components/hello";
-
-ReactDOM.render(<Hello />, document.getElementById("obshome"));
-registerServiceWorker();
