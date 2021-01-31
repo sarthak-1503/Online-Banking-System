e1725e20c3f0b1f435f9462c5d4bac1798b2076a
Sarthak Arora <sarthakarora1503@gmail.com>
2021-02-01 02:21:44 +0530

    OBS react-app

---
+++ react-app/src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { ReactDOM } from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import bootstrap from "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render("",document.querySelector("#root"));
+
+registerServiceWorker();
