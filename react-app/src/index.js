d27bf2ed0cccdaec5c374fd3e623bf4e30a05439
Sarthak Arora <63057512+sarthak-1503@users.noreply.github.com>
2021-04-09 11:49:04 +0530

    Delete react-app directory

--- react-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { ReactDOM } from "react-dom";
-import registerServiceWorker from "./registerServiceWorker";
-import bootstrap from "bootstrap/dist/css/bootstrap.css";
-
-ReactDOM.render("",document.querySelector("#root"));
-
-registerServiceWorker();
