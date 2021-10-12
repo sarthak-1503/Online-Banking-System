0e9b8bc12992c9e328e99cd775aae9c04a3e074c
Sarthak Arora <sarthakarora1503@gmail.com>
2021-10-13 01:43:44 +0530

    resolved some bugs

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,106 +1,121 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
-let Accounts = require('../models/accountModel');
-let requireLogin = require('../middlewares/AuthMiddleware');
+let Accounts = require("../models/accountModel");
+let requireLogin = require("../middlewares/AuthMiddleware");
 
 router.get("/transaction", requireLogin, async (req, res) => {
-
-    let record = await Accounts.findOne({ _id: req.session.user_id });
-    res.render("transaction", { id: req.session.user_id, record: record });
+  let record = await Accounts.findOne({ _id: req.session.user_id });
+  res.render("transaction", { id: req.session.user_id, record: record });
 });
 
 router.get("/create_acc", requireLogin, async (req, res) => {
-
-    let record = await Accounts.findOne({ _id: req.session.user_id });
-    res.render("openaccount", { id: req.session.user_id, record: record });
+  let record = await Accounts.findOne({ _id: req.session.user_id });
+  res.render("openaccount", { id: req.session.user_id, record: record });
 });
 
 router.post("/create_acc", async (req, res) => {
+  var name = req.body.name;
+  var address = req.body.address;
+  var email = req.body.email;
+  var gender = req.body.gender;
+  var Mobileno1 = req.body.Mobileno1;
+  var Mobileno2 = req.body.Mobileno2;
+  var Phoneno = req.body.Phoneno;
+  var total_amount = 0;
 
-    var name = req.body.name;
-    var address = req.body.address;
-    var email = req.body.email;
-    var gender = req.body.gender;
-    var Mobileno1 = req.body.Mobileno1;
-    var Mobileno2 = req.body.Mobileno2;
-    var Phoneno = req.body.Phoneno;
-    var total_amount = 0;
+  let details = {
+    name: name,
+    address: address,
+    email: email,
+    gender: gender,
+    Mobileno1: Mobileno1,
+    Mobileno2: Mobileno2,
+    Phoneno: Phoneno,
+    total_amount: total_amount,
+  };
 
-    let details = {
-        name: name,
-        address: address,
-        email: email,
-        gender: gender,
-        Mobileno1: Mobileno1,
-        Mobileno2: Mobileno2,
-        Phoneno: Phoneno,
-        total_amount: total_amount
+  let record = await Accounts.findOne({ _id: req.session.user_id });
+
+  Accounts.updateOne(record, { $set: details }, (err, rec) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Account Created");
+      res.redirect("/");
     }
-
-    let record = await Accounts.findOne({ _id: req.session.user_id });
-
-    Accounts.updateOne(record, { $set: details }, (err, rec) => {
-        if (err) {
-            console.log(err);
-        }
-        else {
-            console.log('Account Created');
-            res.redirect("/");
-        }
-    });
+  });
 });
 
 router.get("/personaldetails", requireLogin, async (req, res) => {
-
-    let record = await Accounts.findOne({ _id: req.session.user_id });
-    res.render("personaldetails", { id: req.session.user_id, record: record });
+  let record = await Accounts.findOne({ _id: req.session.user_id });
+  res.render("personaldetails", { id: req.session.user_id, record: record });
 });
 
 router.get("/view", async (req, res) => {
-    let record = await Accounts.findOne({ _id: req.session.user_id });
-    let Account = record;
-    res.render("view", { Account: Account, record: record, id: req.session.user_id });
+  let record = await Accounts.findOne({ _id: req.session.user_id });
+  let Account = record;
+  res.render("view", {
+    Account: Account,
+    record: record,
+    id: req.session.user_id,
+  });
 });
 
 router.post("/view", async (req, res) => {
-    var total;
-    var amt = req.body.trans_amount;
-    amt = parseInt(amt);
-    var type = req.body.trans_type;
-    var date = new Date();
-    let record = await Accounts.findOne({ _id: req.session.user_id });
+  var total;
+  var amt = req.body.trans_amount;
+  amt = parseInt(amt);
+  var type = req.body.trans_type;
+  var date = new Date();
+  let record = await Accounts.findOne({ _id: req.session.user_id });
 
-    total = record.total_amount;
-    total = parseInt(total);
-    if (type == "withdrawl") {
-        if (amt > 0 && amt < total) {
-            Accounts.updateOne(record, { $set: { total_amount: total - amt }, $push: { transactions: [{ amount: amt, typeOftransac: type, dateANDtime: date }] } }, function (err, transaction) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(transaction);
-                    res.redirect("/account/view");
-                }
-            });
-
+  total = record.total_amount;
+  total = parseInt(total);
+  if (type == "withdrawl") {
+    if (amt > 0 && amt < total) {
+      Accounts.updateOne(
+        record,
+        {
+          $set: { total_amount: total - amt },
+          $push: {
+            transactions: [
+              { amount: amt, typeOftransac: type, dateANDtime: date },
+            ],
+          },
+        },
+        function (err, transaction) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log(transaction);
+            res.redirect("/account/view");
+          }
         }
-        else {
-            res.render("alert");
+      );
+    } else {
+      res.render("alert", { id: req.session.user_id, record : record });
+    }
+  } else if (type == "deposit") {
+    Accounts.updateOne(
+      record,
+      {
+        $set: { total_amount: total + amt },
+        $push: {
+          transactions: [
+            { amount: amt, typeOftransac: type, dateANDtime: date },
+          ],
+        },
+      },
+      function (err, transaction) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(transaction);
+          res.redirect("/account/view");
         }
-    }
-    else if (type == "deposit") {
-        Accounts.updateOne(record, { $set: { total_amount: (total + amt) }, $push: { transactions: [{ amount: amt, typeOftransac: type, dateANDtime: date }] } }, function (err, transaction) {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log(transaction);
-                res.redirect("/account/view");
-            }
-        });
-    }
-
+      }
+    );
+  }
 });
 
 module.exports = router;
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -9,9 +9,29 @@ router.get("/", async (req, res) => {
         res.render("home", { id: req.session.user_id, record: record });
     }
     else {
-        res.render("home", { id: req.session.user_id });
+        res.render("home", { id: req.session.user_id, record : null });
     }
 
+});
+
+router.get('/transaction', (req,res)=> {
+    
+    res.redirect('/account/transaction');
+});
+
+router.get('/create_acc', (req,res)=> {
+    
+    res.redirect('/account/create_acc');
+});
+
+router.get('/personaldetails', (req,res)=> {
+    
+    res.redirect('/account/personaldetails');
+});
+
+router.get('/view', (req,res)=> {
+    
+    res.redirect('/account/view');
 });
 
 module.exports = router;
