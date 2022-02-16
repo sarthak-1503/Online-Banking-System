3700a381a779d3b0bcec98a9d9e33428fce5e1f8
Sarthak Arora <sarthakarora1503@gmail.com>
2022-02-17 01:55:01 +0530

    little changes

--- build/Release/.deps/Release/obj.target/binding.node.d
+++ build/Release/.deps/Release/obj.target/binding.node.d
@@ -1,1 +1,1 @@
-cmd_Release/obj.target/binding.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=binding.node -o Release/obj.target/binding.node -Wl,--start-group  -Wl,--end-group 
+cmd_Release/obj.target/binding.node := g++ -o Release/obj.target/binding.node -shared -pthread -rdynamic -m64  -Wl,-soname=binding.node -Wl,--start-group  -Wl,--end-group 
